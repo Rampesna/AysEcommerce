f26371f1646f4abbaece0124fc6ec42d9032219b
Talha Can <rampesna@gmail.com>
2021-08-11 13:12:24 +0300

    v0.0.2.8

--- app/Http/Controllers/Api/v1/UserController.php
+++ app/Http/Controllers/Api/v1/UserController.php
@@ -3,35 +3,17 @@
 namespace App\Http\Controllers\Api\v1;
 
 use App\Http\Requests\UserDeleteRequest;
+use App\Http\Requests\UserIndexRequest;
 use App\Http\Requests\UserShowRequest;
 use App\Http\Requests\UserStoreRequest;
 use App\Http\Requests\UserUpdateRequest;
 use App\Interfaces\UserRepositoryInterface;
 use App\Traits\CheckMethod;
-use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use \App\Traits\Response;
 
 class UserController extends Controller
 {
-    /**
-     * @OA\Schema(
-     *      schema="UsersResponseSchema",
-     *      @OA\Property(
-     *          property="id",
-     *          type="integer",
-     *      ),
-     *      @OA\Property(
-     *          property="name",
-     *          type="string",
-     *      ),
-     *      @OA\Property(
-     *          property="email",
-     *          type="string",
-     *      ),
-     * )
-     */
-
     use Response, CheckMethod;
 
     private $userInterface;
@@ -42,63 +24,11 @@ class UserController extends Controller
     }
 
     /**
-     * @OA\Get(
-     *      tags={"User"},
-     *      path="/user",
-     *      summary="Get list of users",
-     *      @OA\Parameter(
-     *          name="page_index",
-     *          description="",
-     *          in = "query",
-     *          required=false,
-     *          @OA\Schema(
-     *              type="integer",
-     *              example="0",
-     *          )
-     *      ),
-     *      @OA\Parameter(
-     *          name="page_size",
-     *          description="",
-     *          in = "query",
-     *          required=false,
-     *          @OA\Schema(
-     *              type="integer",
-     *              example="10",
-     *          )
-     *      ),
-     *      @OA\Parameter(
-     *          name="order_by",
-     *          description="",
-     *          in = "query",
-     *          required=false,
-     *          @OA\Schema(
-     *              type="string",
-     *              example="id",
-     *          )
-     *      ),
-     *      @OA\Parameter(
-     *          name="order_type",
-     *          description="",
-     *          in = "query",
-     *          required=false,
-     *          @OA\Schema(
-     *              type="string",
-     *              example="asc",
-     *          )
-     *      ),
-     *      @OA\Response(
-     *          response=200,
-     *          description="",
-     *          @OA\JsonContent(
-     *            @OA\Property(property="message", type="integer", example="Users skipped 0 and take 10"),
-     *            @OA\Property(property="error", type="string", example="false"),
-     *            @OA\Property(property="code", type="integer", example="200"),
-     *            @OA\Property(property="response", type="array", @OA\Items(ref="#/components/schemas/UsersResponseSchema"))
-     *          ),
-     *      ),
-     *  ),
+     * @method get
+     * @url {version}/user
+     * @param UserIndexRequest $request
      */
-    public function index(Request $request)
+    public function index(UserIndexRequest $request)
     {
         if (!$this->checkMethod($request, ['get'])) return $this->error('Method not allowed', 405);
         return $this->userInterface->index(
@@ -110,31 +40,9 @@ class UserController extends Controller
     }
 
     /**
-     * @OA\Get(
-     *      tags={"User"},
-     *      path="/user/show",
-     *      summary="Get user informations",
-     *      @OA\Parameter(
-     *          name="id",
-     *          description="",
-     *          in = "query",
-     *          required=true,
-     *          @OA\Schema(
-     *              type="integer",
-     *              example="1",
-     *          )
-     *      ),
-     *      @OA\Response(
-     *          response=200,
-     *          description="",
-     *          @OA\JsonContent(
-     *            @OA\Property(property="message", type="integer", example="Get user informations"),
-     *            @OA\Property(property="error", type="string", example="false"),
-     *            @OA\Property(property="code", type="integer", example="200"),
-     *            @OA\Property(property="response", type="object", ref="#/components/schemas/UsersResponseSchema")
-     *          ),
-     *      ),
-     *  ),
+     * @method get
+     * @url {version}/user/show
+     * @param UserShowRequest $request
      */
     public function show(UserShowRequest $request)
     {
@@ -143,28 +51,9 @@ class UserController extends Controller
     }
 
     /**
-     * @OA\Post(
-     *      tags={"User"},
-     *      path="/user",
-     *      summary="Create new user",
-     *      @OA\RequestBody(
-     *          @OA\JsonContent(
-     *            @OA\Property(property="name", type="string"),
-     *            @OA\Property(property="email", type="string"),
-     *            @OA\Property(property="password", type="string"),
-     *          ),
-     *      ),
-     *      @OA\Response(
-     *          response=200,
-     *          description="",
-     *          @OA\JsonContent(
-     *            @OA\Property(property="message", type="integer", example="User saved successfully"),
-     *            @OA\Property(property="error", type="string", example="false"),
-     *            @OA\Property(property="code", type="integer", example="200"),
-     *            @OA\Property(property="response", type="object", ref="#/components/schemas/UsersResponseSchema")
-     *          ),
-     *      ),
-     *  ),
+     * @method post
+     * @url {version}/user
+     * @param UserStoreRequest $request
      */
     public function store(UserStoreRequest $request)
     {
@@ -173,29 +62,9 @@ class UserController extends Controller
     }
 
     /**
-     * @OA\Put(
-     *      tags={"User"},
-     *      path="/user/update",
-     *      summary="Update user",
-     *      @OA\RequestBody(
-     *          @OA\JsonContent(
-     *            @OA\Property(property="id", type="integer"),
-     *            @OA\Property(property="name", type="string"),
-     *            @OA\Property(property="email", type="string"),
-     *            @OA\Property(property="password", type="string"),
-     *          ),
-     *      ),
-     *      @OA\Response(
-     *          response=200,
-     *          description="",
-     *          @OA\JsonContent(
-     *            @OA\Property(property="message", type="integer", example="User saved successfully"),
-     *            @OA\Property(property="error", type="string", example="false"),
-     *            @OA\Property(property="code", type="integer", example="200"),
-     *            @OA\Property(property="response", type="object", ref="#/components/schemas/UsersResponseSchema")
-     *          ),
-     *      ),
-     *  ),
+     * @method put
+     * @url {version}/user
+     * @param UserUpdateRequest $request
      */
     public function update(UserUpdateRequest $request)
     {
@@ -204,30 +73,9 @@ class UserController extends Controller
     }
 
     /**
-     * @OA\Delete(
-     *      tags={"User"},
-     *      path="/user/delete",
-     *      summary="Delete user",
-     *      @OA\Parameter(
-     *          name="id",
-     *          description="",
-     *          in = "query",
-     *          required=true,
-     *          @OA\Schema(
-     *              type="integer",
-     *              example="1",
-     *          )
-     *      ),
-     *      @OA\Response(
-     *          response=200,
-     *          description="",
-     *          @OA\JsonContent(
-     *            @OA\Property(property="message", type="integer", example="User deleted successfully"),
-     *            @OA\Property(property="error", type="string", example="false"),
-     *            @OA\Property(property="code", type="integer", example="200"),
-     *          ),
-     *      ),
-     *  ),
+     * @method delete
+     * @url {version}/user/delete
+     * @param UserDeleteRequest $request
      */
     public function destroy(UserDeleteRequest $request)
     {
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,13 +7,6 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
-/**
- * @OA\Info(
- *     title="aysEcommerce Api",
- *     version="0.0.2.6"
- * )
- * @OA\Server(url="http://127.0.0.1:8000/api/v1/")
- */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
